420c6eacf8663b63727044588e8141bbbfa9d6a8
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-21 23:11:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! I BUILT THIS! Can you believe it? "
+    "Started my server using Shotgun!"
   end
 
 end
